2ec31878ecc89060fba3807a5b7e2b1ff51fe240
Emil Varghese <emilvpulinattu@gmail.com>
2016-07-12 23:09:12 +0000

    Add user details and user_id

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,8 @@
 class Article < ActiveRecord::Base
-    validates :title, presence:true, length:{minimum:3,maximum:40 }
-    validates :description, presence:true, length:{minimum:10,maximum:300}
+    belongs_to :user
     
+    validates :title, presence: true, length: {minimum:3,maximum:40 }
+    validates :description, presence: true, length: {minimum:10,maximum:300}
+    validates :user_id, presence: true    
 end
+
